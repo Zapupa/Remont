68f7f16170f97c798cd96bfabf9d334fb5f7d39c
Zapupa <vitaliy2005rus93@gmail.com>
2025-05-18 10:20:18 +0500

    Логин

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration {
             $table->string('tel');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role');
+            $table->string('role')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
